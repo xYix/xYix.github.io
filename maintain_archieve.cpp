1399e05f5a547818ed85d418371ab68d039565f6
xYix <1074071617@qq.com>
2020-10-03 11:35:23 +0800

    ZKAK

--- maintain_archieve.cpp
+++ maintain_archieve.cpp
@@ -34,6 +34,29 @@ char* GBToUTF8(const char* gb2312)
 	return str;
 }
 
+void PRINT_POST_INFO(){
+	cout<<"<tr><th><a href=\"/posts/\""<<post_name<<".html\"><strong>"<<post_chinese_name<<"</strong></a></th>\n";
+	
+	if(type_name=="solution")
+		cout<<"<th><a href=\"/archieve/solution/\"><strong>题解</strong></a></th>\n";
+	else if(type_name=="algorithm")
+		cout<<"<th><a href=\"/archieve/algorithm/\"><strong>算法</strong></a></th>\n";
+	else
+		cout<<"<th><p>无</p></th>\n";
+	
+	if(tag_cnt==0) cout<<"<th><p>无</p></th>\n";
+	else{
+		cout<<"<th>";
+		for(int i=1;i<=tag_cnt;i++){
+			cout<<"<a href=\"/archieve/"<<post_tag[i]<<"/\"><strong>";
+			if(!TAG.count(post_tag[i])){cerr<<"ERROR tag not found\n";exit(0);}
+			cout<<TAG[post_tag[i]]<<"</strong></a>";
+			if(i!=tag_cnt) cout<<" ";
+		}
+		cout<<"</th>\n";
+	}
+}
+
 int main(){
 	/*
 		list 格式：
@@ -52,12 +75,12 @@ int main(){
 		TAG[tmp_tag_name]=tmp_tag_chinese_name;
 	}
 	cin.clear();
-	freopen("D:\\迫真blog\\posts\\list.txt","r",stdin);
-	freopen("D:\\迫真blog\\archieve\\index0.txt","w",stdout);
+	freopen("D:\\迫真blog\\archieve\\list.txt","r",stdin);
+	freopen("D:\\迫真blog\\archieve\\index.html","w",stdout);
 	cout<<"<head><meta http-equiv=\"content-type\" content=\"text/html;charset=gbk\" /><meta charset=\"UTF-8\"><title>x义x 的 blog - 文章一览</title><link rel=\"icon\" type=\"images/png\" sizes=\"32x32\" href=\"/images/favicon32.png\"><link rel=\"icon\" type=\"images/png\" sizes=\"32x32\" href=\"/images/favicon16.png\"><link rel=\"stylesheet\" href=\"/css/main.css\"></head>\n";
 	cout<<"<div class=\"cover\"></div><div class=\"title\"><h1>x义x 的自制 BLOG</h1><p>您现在在：文章一览</p></div>\n";
 	cout<<"<div class=\"bodybody\"><div class=\"sidebar\"><div class=\"content-block\"><div class=\"content\"><center><p>……就算是单程票，也是可以的吧？</p></center><a href=\"/\"><strong>· 回到首页</strong></a><p></p><a href=\"/archieve/\"><strong>· 文章一览</strong></a><p></p><a href=\"/tags/\"><strong>· 标签一览</strong></a><p></p><a href=\"/songlist/\"><strong>· 网义云音乐</strong></a></div></div></div>\n";
-	cout<<"<div class=\"post-block\"><div class=\"content-block\"><center><h1>文章一览</h1></center><center><h2>· 2020/10</h2></center><center><table border=\"1\" style=\"width: 100%;\"><tr><th>标题</th><th>分类</th><th>标签</th></tr>\n";
+	cout<<"<div class=\"post-block\"><div class=\"content-block\"><center><h1>文章一览</h1></center><center><h2>· 2020/10</h2></center><center><table border=\"1\" style=\"width: 100%;\"><tr><th style=\"width:35%\">标题</th><th style=\"width:20%\">分类</th><th style=\"width:45%\">标签</th></tr>\n";
 	while(getline(cin,post_name)){
 		cerr<<"check "<<post_name<<"\n";
 		getline(cin,post_chinese_name);
@@ -65,24 +88,7 @@ int main(){
 		getline(cin,type_name);
 		cin>>tag_cnt;getline(cin,trash);
 		for(int i=1;i<=tag_cnt;i++) getline(cin,post_tag[i]);
-		cout<<"<tr><th><a href=\"/posts/\""<<post_name<<".html\"><strong>"<<post_chinese_name<<"</strong></a></th>\n";
-		if(type_name=="solution")
-			cout<<"<th><a href=\"/archieve/solution/\"><strong>题解</strong></a></th>\n";
-		else if(type_name=="algorithm")
-			cout<<"<th><a href=\"/archieve/algorithm/\"><strong>算法</strong></a></th>\n";
-		else
-			cout<<"<th><p>无</p></th>\n";
-		if(tag_cnt==0) cout<<"<th><p>无</p></th>\n";
-		else{
-			cout<<"<th>";
-			for(int i=1;i<=tag_cnt;i++){
-				cout<<"<a href=\"/archieve/"<<post_tag[i]<<"/\"><strong>";
-				if(!TAG.count(post_tag[i])){cerr<<"ERROR tag not found\n";exit(0);}
-				cout<<TAG[post_tag[i]]<<"</strong></a>";
-				if(i!=tag_cnt) cout<<" ";
-			}
-			cout<<"</th>\n";
-		}
+		PRINT_POST_INFO();
 	}
 	cout<<"</table></center></div></div></div>\n";
 }
--- maintain_tags.cpp
+++ maintain_tags.cpp
@@ -2,10 +2,7 @@
 #include<windows.h>
 using namespace std;
 
-string post_name,post_chinese_name,type_name;
-string post_tag[20];int tag_cnt;
-map<string,string> TAG;
-string trash;
+string tag_name,tag_chinese_name;
 
 char* UTF8ToGB(const char* utf8)
 {
@@ -34,6 +31,10 @@ char* GBToUTF8(const char* gb2312)
 	return str;
 }
 
+void PRINT_TAG_INFO(){
+	cout<<"<tr><th><a href=\"/tags/"<<tag_name<<"/\"><strong>"<<tag_chinese_name<<"</strong></a></th><th>"<<tag_name<<"</th></tr>\n";
+} 
+
 int main(){
 	/*
 		list 格式：
@@ -45,44 +46,16 @@ int main(){
 	*/
 	
 	freopen("D:\\迫真blog\\tags\\list.txt","r",stdin);
-	string tmp_tag_name,tmp_tag_chinese_name;
-	while(getline(cin,tmp_tag_name)){
-		getline(cin,tmp_tag_chinese_name);
-		tmp_tag_chinese_name=UTF8ToGB(tmp_tag_chinese_name.c_str());
-		TAG[tmp_tag_name]=tmp_tag_chinese_name;
-	}
-	cin.clear();
-	freopen("D:\\迫真blog\\posts\\list.txt","r",stdin);
-	freopen("D:\\迫真blog\\archieve\\index.html","w",stdout);
+	freopen("D:\\迫真blog\\tags\\index.html","w",stdout);
 	cout<<"<head><meta http-equiv=\"content-type\" content=\"text/html;charset=gbk\" /><meta charset=\"UTF-8\"><title>x义x 的 blog - 文章一览</title><link rel=\"icon\" type=\"images/png\" sizes=\"32x32\" href=\"/images/favicon32.png\"><link rel=\"icon\" type=\"images/png\" sizes=\"32x32\" href=\"/images/favicon16.png\"><link rel=\"stylesheet\" href=\"/css/main.css\"></head>\n";
-	cout<<"<div class=\"cover\"></div><div class=\"title\"><h1>x义x 的自制 BLOG</h1><p>您现在在：文章一览</p></div>\n";
+	cout<<"<div class=\"cover\"></div><div class=\"title\"><h1>x义x 的自制 BLOG</h1><p>您现在在：标签一览</p></div>\n";
 	cout<<"<div class=\"bodybody\"><div class=\"sidebar\"><div class=\"content-block\"><div class=\"content\"><center><p>……就算是单程票，也是可以的吧？</p></center><a href=\"/\"><strong>· 回到首页</strong></a><p></p><a href=\"/archieve/\"><strong>· 文章一览</strong></a><p></p><a href=\"/tags/\"><strong>· 标签一览</strong></a><p></p><a href=\"/songlist/\"><strong>· 网义云音乐</strong></a></div></div></div>\n";
-	cout<<"<div class=\"post-block\"><div class=\"content-block\"><center><h1>文章一览</h1></center><center><h2>· 2020/10</h2></center><center><table border=\"1\" style=\"width: 100%;\"><tr><th>标题</th><th>分类</th><th>标签</th></tr>\n";
-	while(getline(cin,post_name)){
-		cerr<<"check "<<post_name<<"\n";
-		getline(cin,post_chinese_name);
-		post_chinese_name=UTF8ToGB(post_chinese_name.c_str());
-		getline(cin,type_name);
-		cin>>tag_cnt;getline(cin,trash);
-		for(int i=1;i<=tag_cnt;i++) getline(cin,post_tag[i]);
-		cout<<"<tr><th><a href=\"/posts/\""<<post_name<<".html\"><strong>"<<post_chinese_name<<"</strong></a></th>\n";
-		if(type_name=="solution")
-			cout<<"<th><a href=\"/archieve/solution/\"><strong>题解</strong></a></th>\n";
-		else if(type_name=="algorithm")
-			cout<<"<th><a href=\"/archieve/algorithm/\"><strong>算法</strong></a></th>\n";
-		else
-			cout<<"<th><p>无</p></th>\n";
-		if(tag_cnt==0) cout<<"<th><p>无</p></th>\n";
-		else{
-			cout<<"<th>";
-			for(int i=1;i<=tag_cnt;i++){
-				cout<<"<a href=\"/archieve/"<<post_tag[i]<<"/\"><strong>";
-				if(!TAG.count(post_tag[i])){cerr<<"ERROR tag not found\n";exit(0);}
-				cout<<TAG[post_tag[i]]<<"</strong></a>";
-				if(i!=tag_cnt) cout<<" ";
-			}
-			cout<<"</th>\n";
-		}
+	cout<<"<div class=\"post-block\"><div class=\"content-block\"><center><h1>标签一览</h1></center><center><table border=\"1\" style=\"width: 70%;\"><tr><th style=\"width: 40%\">名称</th><th style=\"width: 60%\">英文名</th></tr>\n";
+	while(getline(cin,tag_name)){
+		cerr<<"check "<<tag_name<<"\n";
+		getline(cin,tag_chinese_name);
+		tag_chinese_name=UTF8ToGB(tag_chinese_name.c_str());
+		PRINT_TAG_INFO();
 	}
 	cout<<"</table></center></div></div></div>\n";
 }
