8851b64c46e4150c1d38adfa0154a1b55db77cd1
xYix <1074071617@qq.com>
2020-10-04 20:38:32 +0800

    ver 1.0.0

--- js/basic.js
+++ js/basic.js
@@ -1,6 +1,6 @@
 (function(win){
     'use strict',
-    // 格式： xyix.gitee.io/.../.../?tags=...+...&type=...&sortby=
+    // 格式： xyix.gitee.io/.../.../?tags=...+...&type=...&sortby=&page=
     win.AnalyzeSearch = function (s){
         let ret = {}, t, r;
 		for (t of (s.startsWith('?') ? s.substr(1) : s).split('&'))
@@ -11,7 +11,7 @@
     win.AnalyzePathname = function (s){
         let ret = [], t;
         for (t of (s.startsWith('/') ? s.substr(1) : s).split('/'))
-            if(t.match('[.]') === null) ret[ret.length] = t;
+            if(t.match('[.]') === null && t !== 'D:' && t !== '%E8%BF%AB%E7%9C%9Fblog') ret[ret.length] = t;
 		return ret;
     }
     win.AnalyzeTags = function (s){
@@ -78,6 +78,9 @@
     win.Tags=win.AnalyzeTags(win.Search['tags']);
     win.Type=win.Search['type'];
     win.Sortby=win.Search['sortby'];
+    win.Page=win.Search['page'];
+    if(win.Page === undefined) win.Page = 0;
+    // Title
     win.Title='';
     if(win.Pathname.length === 0) win.Title='首页';
     else{
@@ -85,7 +88,7 @@
         if(win.Pathname[0] === 'tags') win.Title='标签一览';
         if(win.Pathname[0] === 'songlist') win.Title='网义云音乐';
         if(win.Pathname[0] === 'archieve'){
-            win.Title += '，';
+            win.Title += '：';
             if(win.Type !== undefined){
                 win.Title += '分类为：';
                 if(win.Type == 'solution') win.Title += '题解';
@@ -107,4 +110,39 @@
         }
     }
     win.title='x义x 的 blog - ' + win.Title;
+    win.WriteTitle2 = function (data){
+        let AddText = function (twin,tdata,ttext,eletag){
+            let Ttext=twin.createElement(eletag);
+            Ttext.textContent = ttext;
+            let TTtext=twin.createElement('center');
+            TTtext.appendChild(Ttext);
+            tdata.appendChild(TTtext);
+        }
+        if(win.Pathname.length === 0) AddText(win,data,'公告','h1');
+        else{
+            if(win.Pathname[0] === 'archieve') AddText(win,data,'文章一览','h1');
+            if(win.Pathname[0] === 'tags') AddText(win,data,'标签一览','h1');
+            if(win.Pathname[0] === 'songlist') AddText(win,data,'网义云音乐','h1');
+            if(win.Pathname[0] === 'archieve'){
+                if(win.Type !== undefined){
+                    let Typeinfo = '分类为：';
+                    if(win.Type == 'solution') Typeinfo += '题解';
+                    else if(win.Type == 'algorithm') Typeinfo += '算法/知识点';
+                    else if(win.Type == 'other') Typeinfo += '游记/其他';
+                    else Typeinfo += '不明分类';
+                    AddText(win,data,Typeinfo,'h3');
+                }
+                if(win.Tags.length !== 0){
+                    let Taginfo = '具有标签：';
+                    for (let i = 0; i < win.Tags.length; i = i + 1){
+                        if(win.tags_list[win.Tags[i]] !== undefined)
+                            Taginfo += win.tags_list[win.Tags[i]];
+                        else Taginfo += '不明标签';
+                        if(i !== win.Tags.length - 1) Taginfo += '，';
+                    }
+                    AddText(win,data,Taginfo,'h3');
+                }
+            }
+        }
+    }
 })(document);
