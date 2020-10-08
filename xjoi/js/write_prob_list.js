5f0eb88a7b2975c3872cdf66e670f8deee54e6c3
xYix <1074071617@qq.com>
2020-10-08 09:58:35 +0800

    xjoi-probset

--- xjoi/js/write_prob_list.js
+++ xjoi/js/write_prob_list.js
@@ -7,12 +7,17 @@
             Probinfo_id_p.textContent=Probid;
             Probinfo_id.appendChild(Probinfo_id_p);
         ProbinfoBlock.appendChild(Probinfo_id);
-            let Probinfo_name=win.createElement('tr');
+            let Probinfo_name=win.createElement('th');
             let Probinfo_name_a=win.createElement('a');
             Probinfo_name_a.href='/xjoi/probs/'+Probname+'.html';
             Probinfo_name_a.textContent=win.prob_list[Probname];
             Probinfo_name.appendChild(Probinfo_name_a);
         ProbinfoBlock.appendChild(Probinfo_name);
+            let Probinfo_path=win.createElement('th');
+            let Probinfo_path_p=win.createElement('p');
+            Probinfo_path_p.textContent=Probname;
+            Probinfo_path.appendChild(Probinfo_path_p);
+        ProbinfoBlock.appendChild(Probinfo_path);
         data.appendChild(ProbinfoBlock);
     }
     win.isLegalProb = function(Probname,prob_count){
@@ -32,9 +37,13 @@
                 Titleh0.appendChild(win.createTextNode('编号'));
             ProblemSetTitle.appendChild(Titleh0);
                 let Titleh1=win.createElement('th');
-                Titleh1.style='width: 60%';
+                Titleh1.style='width: 50%';
                 Titleh1.appendChild(win.createTextNode('标题'));
             ProblemSetTitle.appendChild(Titleh1);
+                let Titleh2=win.createElement('th');
+                Titleh2.style='width: 40%';
+                Titleh2.appendChild(win.createTextNode('目录名'));
+            ProblemSetTitle.appendChild(Titleh2);
         ProblemSetTable.appendChild(ProblemSetTitle);
         for(var Probname in win.prob_list)
             if(win.isLegalProb(Probname,win.prob_count))
