ae9250cb54d8b66c8177b270cded900ee12a1207
xYix <1074071617@qq.com>
2020-12-02 15:03:36 +0800

    add last_modi

--- js/basic.js
+++ js/basic.js
@@ -417,7 +417,7 @@
             Titleh0.appendChild(win.createTextNode('编号'));
         ArchieveTitle.appendChild(Titleh0);
             let Titleh1=win.createElement('th');
-            Titleh1.style='width: calc(50% - 8.5em - 30px)';
+            Titleh1.style='width: calc(0.63 * (100% - 17em - 60px))';
             Titleh1.appendChild(win.createTextNode('标题'));
         ArchieveTitle.appendChild(Titleh1);
             let Titleh2=win.createElement('th');
@@ -425,7 +425,7 @@
             Titleh2.appendChild(win.createTextNode('分类'));
         ArchieveTitle.appendChild(Titleh2);
             let Titleh3=win.createElement('th');
-            Titleh3.style='width: calc(50% - 8.5em - 30px)';
+            Titleh3.style='width: calc(0.37 * (100% - 17em - 60px))';
             Titleh3.appendChild(win.createTextNode('标签'));
         ArchieveTitle.appendChild(Titleh3);
             let Titleh4=win.createElement('th');
