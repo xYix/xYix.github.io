f89e6ee0c0ce6ee77af3722b241b9e3256259797
xYix <1074071617@qq.com>
2021-02-20 08:02:06 +0800

    essential

--- js/basic.js
+++ js/basic.js
@@ -153,8 +153,8 @@
         data.appendChild(SideBar);
     }
     win.findpost = function (postname) {
-        for (var postinfo in win.archieve_list)
-            if (postinfo.post_name === postname) return postinfo;
+        for (var i = 0; i < win.archieve_list.length; i = i + 1)
+            if (win.archieve_list[i].post_name === postname) return win.archieve_list[i];
         return 'error';
     }
     // Title
