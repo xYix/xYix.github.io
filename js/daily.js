519a64694437d2724dcf1e9941123a45ca291779
xYix <1074071617@qq.com>
2021-03-30 15:35:14 +0800

    tuu

--- js/daily.js
+++ js/daily.js
@@ -411,12 +411,22 @@
     ];
     win.Write_Daily_Message = function (data){
         let message_box=document.createElement('center');
-        let message_title=document.createElement('h3');
-        message_title.textContent="一言";
-        let daily_message=document.createElement('p');
-        daily_message.textContent=DAILY_MESSAGES[Math.floor(Math.random()*DAILY_MESSAGES.length)];
-        message_box.appendChild(message_title);
-        message_box.appendChild(daily_message);
+        if (Math.floor(Math.random() * 5) != 0) {
+            let message_title=document.createElement('h3');
+            message_title.textContent="一言";
+            let daily_message=document.createElement('p');
+            daily_message.textContent=DAILY_MESSAGES[Math.floor(Math.random()*DAILY_MESSAGES.length)];
+            message_box.appendChild(message_title);
+            message_box.appendChild(daily_message);
+        }
+        else {
+            let message_title=document.createElement('h3');
+            message_title.textContent="广告";
+            let daily_message=document.createElement('p');
+            daily_message.textContent = '广告位招租';
+            message_box.appendChild(message_title);
+            message_box.appendChild(daily_message);
+        }
         data.appendChild(message_box);
     }
 })(document);
