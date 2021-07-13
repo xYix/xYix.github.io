012619df55b3085c79caf30c03c4d950e0fed0ee
xYix <1074071617@qq.com>
2021-07-13 20:08:57 +0800

    qaq

--- js/archieve_list.js
+++ js/archieve_list.js
@@ -1112,6 +1112,14 @@
 		'last_modi' : '2021-07-13',
 		'tag' : ['keter','lmoliver','submit-answer',],
 	};
+	archieve_list[archieve_list.length] = {
+		'postid' : '140',
+		'post_name' : 'LNR #2 题解',
+		'post_chinese_name' : 'solution',
+		'type_name' : '4',
+		'last_modi' : '',
+		'tag' : [],
+	};
 	for (var i in archieve_list) archieve_list[i].tag.sort(function(a, b){ return tags_val[a] - tags_val[b]});
 for (var i in archieve_list) {
 let u = archieve_list[i], qaq = u.last_modi.split('-');
