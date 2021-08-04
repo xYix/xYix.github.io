a07f2b673fa04955058d9543342abb796440a90a
xYix <1074071617@qq.com>
2021-08-05 07:46:55 +0800

    qaq

--- js/load_theme.js
+++ js/load_theme.js
@@ -2,22 +2,22 @@
     'use strict';
     if (win.ThemeColor === undefined || win.ThemeColor === 'Z') {
         if (win.Funval === 'sayonara')
-            win.querySelector('body').style='--main-color: #1a5a40; --dark-main-color: #36c48e';
-        else win.querySelector('body').style='--main-color: #36c48e; --dark-main-color: #1a5a40';
+            win.querySelector('body').style='--main-color: #1a5a40; --dark-main-color: #36c48e; --text-color: #aaaaaa';
+        else win.querySelector('body').style='--main-color: #36c48e; --dark-main-color: #1a5a40; --text-color: #555555';
     }
     if (win.ThemeColor === 'Y') {
         if (win.Funval === 'sayonara')
-            win.querySelector('body').style='--main-color: #753432; --dark-main-color: #ea6965';
-        else win.querySelector('body').style='--main-color: #ea6965; --dark-main-color: #753432';
+            win.querySelector('body').style='--main-color: #753432; --dark-main-color: #ea6965; --text-color: #aaaaaa';
+        else win.querySelector('body').style='--main-color: #ea6965; --dark-main-color: #753432; --text-color: #555555';
     }
     if (win.ThemeColor === 'X') {
         if (win.Funval === 'sayonara')
-            win.querySelector('body').style='--main-color: #1e5f77; --dark-main-color: #3dbeee';
-        else win.querySelector('body').style='--main-color: #3dbeee; --dark-main-color: #1e5f77';
+            win.querySelector('body').style='--main-color: #1e5f77; --dark-main-color: #3dbeee; --text-color: #aaaaaa';
+        else win.querySelector('body').style='--main-color: #3dbeee; --dark-main-color: #1e5f77; --text-color: #555555';
     }
     if (win.ThemeColor === 'N') {
         if (win.Funval === 'sayonara')
-            win.querySelector('body').style='--main-color: #7f415b; --dark-main-color: #ff83b7';
-        else win.querySelector('body').style='--main-color: #ff83b7; --dark-main-color: #7f415b';
+            win.querySelector('body').style='--main-color: #7f415b; --dark-main-color: #ff83b7; --text-color: #aaaaaa';
+        else win.querySelector('body').style='--main-color: #ff83b7; --dark-main-color: #7f415b; --text-color: #555555';
     }
 })(document);
