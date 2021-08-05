08a6f925e6fabc1193e827dd40ac3b640a957eea
xYix <1074071617@qq.com>
2021-08-05 18:57:26 +0800

    fuck

--- js/load_theme.js
+++ js/load_theme.js
@@ -1,24 +1,32 @@
 (function (win) {
     'use strict';
+    let winbody = win.querySelector('body');
     if (win.ThemeColor === undefined || win.ThemeColor === 'Z') {
         if (win.Funval === 'sayonara')
-            win.querySelector('body').style='--main-color: #1a5a40; --dark-main-color: #36c48e; --text-color: #bbbbbb; --opa: 0.3';
-        else win.querySelector('body').style='--main-color: #36c48e; --dark-main-color: #1a5a40; --text-color: #555555; --opa: 0.7';
+            winbody.style='--main-color: #1a5a40; --dark-main-color: #36c48e;';
+        else winbody.style='--main-color: #36c48e; --dark-main-color: #1a5a40;';
     }
     if (win.ThemeColor === 'Y') {
         if (win.Funval === 'sayonara')
-            win.querySelector('body').style='--main-color: #753432; --dark-main-color: #ea6965; --text-color: #bbbbbb; --opa: 0.3';
-        else win.querySelector('body').style='--main-color: #ea6965; --dark-main-color: #753432; --text-color: #555555; --opa: 0.7';
+            winbody.style='--main-color: #753432; --dark-main-color: #ea6965;';
+        else winbody.style='--main-color: #ea6965; --dark-main-color: #753432;';
     }
     if (win.ThemeColor === 'X') {
         if (win.Funval === 'sayonara')
-            win.querySelector('body').style='--main-color: #1e5f77; --dark-main-color: #3dbeee; --text-color: #bbbbbb; --opa: 0.3';
-        else win.querySelector('body').style='--main-color: #3dbeee; --dark-main-color: #1e5f77; --text-color: #555555; --opa: 0.7';
+            winbody.style='--main-color: #1e5f77; --dark-main-color: #3dbeee;';
+        else winbody.style='--main-color: #3dbeee; --dark-main-color: #1e5f77;';
     }
     if (win.ThemeColor === 'N') {
         if (win.Funval === 'sayonara')
-            win.querySelector('body').style='--main-color: #7f415b; --dark-main-color: #ff83b7; --text-color: #bbbbbb; --opa: 0.3';
-        else win.querySelector('body').style='--main-color: #ff83b7; --dark-main-color: #7f415b; --text-color: #555555; --opa: 0.7';
+            winbody.style='--main-color: #7f415b; --dark-main-color: #ff83b7;';
+        else winbody.style='--main-color: #ff83b7; --dark-main-color: #7f415b;';
     }
-    console.log(win.querySelector('body').style['--main-color']);
+    if (win.Funval === 'sayonara')
+        winbody.style.setProperty("--opa", 0.3),
+        winbody.style.setProperty("--text-color", '#bbbbbb'),
+        winbody.style.setProperty("--outside-display", 'none');
+    else 
+        winbody.style.setProperty("--opa", 0.7),
+        winbody.style.setProperty("--text-color", '#555555'),
+        winbody.style.setProperty("--outside-display", 'default');
 })(document);
