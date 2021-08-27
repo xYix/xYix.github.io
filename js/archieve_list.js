cc3332d9aea9f3df4ec437b1599007da58b5850f
xYix <1074071617@qq.com>
2021-08-27 09:35:18 +0800

    qaq

--- js/archieve_list.js
+++ js/archieve_list.js
@@ -1352,6 +1352,14 @@
 		'last_modi' : '2021-08-26',
 		'tag' : ['euclid','game-theory',],
 	};
+	archieve_list[archieve_list.length] = {
+		'postid' : '170',
+		'post_name' : 'luogu-7054',
+		'post_chinese_name' : 'luoguP7054 题解 - 【NWRRC2015】Graph',
+		'type_name' : 'solution',
+		'last_modi' : '2021-08-27',
+		'tag' : ['graph-theory','keter',],
+	};
 	for (var i in archieve_list) archieve_list[i].tag.sort(function(a, b){ return tags_val[a] - tags_val[b]});
 for (var i in archieve_list) {
 let u = archieve_list[i], qaq = u.last_modi.split('-');
