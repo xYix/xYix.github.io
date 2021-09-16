24e715f0f69899341c4b268e87392e887d455eb2
xYix <1074071617@qq.com>
2021-09-16 15:50:50 +0800

    qaq

--- js/archieve_list.js
+++ js/archieve_list.js
@@ -1325,7 +1325,7 @@
 		'post_name' : 'recent-kouhu',
 		'post_chinese_name' : '近期嘴巴杂记',
 		'type_name' : 'solution',
-		'last_modi' : '2021-09-13',
+		'last_modi' : '2021-09-16',
 		'tag' : ['data-structure','geometry','graph-theory','keter','linear-algebra',],
 	};
 	archieve_list[archieve_list.length] = {
@@ -1406,7 +1406,15 @@
 		'post_chinese_name' : '论文翻译：钩长公式的一个概率论证明',
 		'type_name' : 'algorithm',
 		'last_modi' : '2021-09-15',
-		'tag' : ['combinatorics',],
+		'tag' : ['luogu-4502',],
+	};
+	archieve_list[archieve_list.length] = {
+		'postid' : '177',
+		'post_name' : 'luoguP4502 题解 - 【ZJOI2018】保镖',
+		'post_chinese_name' : 'solution',
+		'type_name' : '2',
+		'last_modi' : '',
+		'tag' : [],
 	};
 	for (var i in archieve_list) archieve_list[i].tag.sort(function(a, b){ return tags_val[a] - tags_val[b]});
 for (var i in archieve_list) {
