b21408faf24db0ccd1987032535de5f82bb165ab
xYix <1074071617@qq.com>
2021-10-13 08:15:28 +0800

    qaq

--- js/archieve_list.js
+++ js/archieve_list.js
@@ -1464,6 +1464,14 @@
 		'last_modi' : '2021-10-12',
 		'tag' : ['keter','lagrange-inversion','polynomial',],
 	};
+	archieve_list[archieve_list.length] = {
+		'postid' : '184',
+		'post_name' : 'cf-1237-g',
+		'post_chinese_name' : 'CF1237G 题解 - Balanced Distribution',
+		'type_name' : 'solution',
+		'last_modi' : '2021-10-13',
+		'tag' : ['eculid','optimization',],
+	};
 	for (var i in archieve_list) archieve_list[i].tag.sort(function(a, b){ return tags_val[a] - tags_val[b]});
 for (var i in archieve_list) {
 let u = archieve_list[i], qaq = u.last_modi.split('-');
