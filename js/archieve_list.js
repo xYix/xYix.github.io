698ab34bcbc23f2e631968476cfed979ed4763e9
xYix <1074071617@qq.com>
2021-10-18 20:36:22 +0800

    qaq

--- js/archieve_list.js
+++ js/archieve_list.js
@@ -5,7 +5,7 @@
 		'post_name' : 'hello-world',
 		'post_chinese_name' : '关于 x义x',
 		'type_name' : 'none',
-		'last_modi' : '2021-10-16',
+		'last_modi' : '2021-10-18',
 		'tag' : [],
 	};
 	archieve_list[archieve_list.length] = {
@@ -1461,7 +1461,7 @@
 		'post_name' : 'luogu-7896',
 		'post_chinese_name' : 'luoguP7896 题解 - Moke 的游戏',
 		'type_name' : 'solution',
-		'last_modi' : '2021-10-12',
+		'last_modi' : '2021-10-18',
 		'tag' : ['keter','lagrange-inversion','polynomial',],
 	};
 	archieve_list[archieve_list.length] = {
@@ -1495,6 +1495,14 @@
 		'type_name' : 'algorithm',
 		'last_modi' : '2021-10-16',
 		'tag' : ['bijective-proof','combinatorics','generating-function',],
+	};
+	archieve_list[archieve_list.length] = {
+		'postid' : '188',
+		'post_name' : 'cf-1586',
+		'post_chinese_name' : 'CFR#749 部分题解',
+		'type_name' : 'solution',
+		'last_modi' : '2021-10-18',
+		'tag' : ['construction','euclid','lmoliver',],
 	};
 	for (var i in archieve_list) archieve_list[i].tag.sort(function(a, b){ return tags_val[a] - tags_val[b]});
 for (var i in archieve_list) {
