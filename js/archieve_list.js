de9cc304d719cbf8e5fd5fd700b78b4270807e33
xYix <1074071617@qq.com>
2022-09-24 18:50:45 +0800

    qaq

--- js/archieve_list.js
+++ js/archieve_list.js
@@ -1725,7 +1725,7 @@
 		'post_name' : 'restart',
 		'post_chinese_name' : 'restart',
 		'type_name' : 'library',
-		'last_modi' : '2022-08-13',
+		'last_modi' : '2022-09-24',
 		'tag' : ['writing',],
 	};
 	archieve_list[archieve_list.length] = {
