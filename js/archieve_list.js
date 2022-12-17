4fad628ddc139740b68ef84e3a116517bc272ba1
xYix <1074071617@qq.com>
2022-12-17 10:29:15 +0800

    qaq

--- js/archieve_list.js
+++ js/archieve_list.js
@@ -1830,7 +1830,7 @@
 		'post_chinese_name' : '奔月',
 		'type_name' : 'library',
 		'last_modi' : '2022-12-17',
-		'tag' : ['andromeda-signal','hentai','suicide',],
+		'tag' : ['andromeda-signal','ero','suicide',],
 	};
 	for (var i in archieve_list) archieve_list[i].tag.sort(function(a, b){ return tags_val[a] - tags_val[b]});
 for (var i in archieve_list) {
