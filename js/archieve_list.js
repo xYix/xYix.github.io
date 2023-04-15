0fe0ab1628c98c17b604b088c48fa5bb6a2c5959
xYix <1074071617@qq.com>
2023-04-15 12:48:55 +0800

    qaq

--- js/archieve_list.js
+++ js/archieve_list.js
@@ -1904,6 +1904,14 @@
 		'last_modi' : '2023-04-05',
 		'tag' : ['politics',],
 	};
+	archieve_list[archieve_list.length] = {
+		'postid' : '239',
+		'post_name' : '4-15',
+		'post_chinese_name' : '后世',
+		'type_name' : 'other',
+		'last_modi' : '2023-04-15',
+		'tag' : ['flowers',],
+	};
 	for (var i in archieve_list) archieve_list[i].tag.sort(function(a, b){ return tags_val[a] - tags_val[b]});
 for (var i in archieve_list) {
 let u = archieve_list[i], qaq = u.last_modi.split('-');
